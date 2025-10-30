451d8f19edec91253741e21a06df1c533723b6b8
ntnganthcshungvuong-art <ntngan.thcshungvuong@gmail.com>
2025-10-30 10:39:38 +0700

    Update quizz.js

--- quizz.js
+++ quizz.js
@@ -1,1 +1,35 @@
+async function loadQuiz() {
+  const res = await fetch('questions.json');
+  const questions = await res.json();
+  const quizDiv = document.getElementById('quiz');
+  
+  questions.forEach((q, i) => {
+    const div = document.createElement('div');
+    div.classList.add('question');
+    div.innerHTML = `<p><b>Câu ${i+1}.</b> ${q.question}</p>`;
+    
+    q.options.forEach((opt, j) => {
+      div.innerHTML += `
+        <label>
+          <input type="radio" name="q${i}" value="${j}">
+          ${opt}
+        </label><br>`;
+    });
+    quizDiv.appendChild(div);
+  });
+
+  document.getElementById('submit').onclick = () => grade(questions);
+}
+
+function grade(questions) {
+  let score = 0;
+  questions.forEach((q, i) => {
+    const chosen = document.querySelector(`input[name="q${i}"]:checked`);
+    if (chosen && parseInt(chosen.value) === q.answer) score++;
+  });
+  document.getElementById('result').innerHTML =
+    `Bạn được ${score}/${questions.length} điểm`;
+}
+
+loadQuiz();
 
